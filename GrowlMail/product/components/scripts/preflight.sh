c639bcbdb866742a4c94adf570c3cc268a3459ec
Rudy Richter <rarichter@gmail.com>
2012-12-23 13:47:25 -0500

    project packaging changes and update the sparkle reference to the latest version

---
+++ GrowlMail/product/components/scripts/preflight.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#####
+# We politely asked the user to quit Mail in the installer intro.  Now
+# we'll request the same a bit more strongly.
+####
+osascript -e "quit app \"Mail\""
+
+# Delete any old copies of the bundle
+rm -rf ~/Library/Mail/Bundles/GrowlMail.mailbundle
+rm -rf /Library/Mail/Bundles/GrowlMail.mailbundle
